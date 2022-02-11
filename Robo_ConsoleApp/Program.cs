a0f7709df76e1f290757529c3fb2cfc30ffb3b93
Kauan Santos <Kauan.h.santos@hotmail.com>
2022-02-11 11:38:35 -0300

    apagando espaço em branco

--- Robo_ConsoleApp/Program.cs
+++ Robo_ConsoleApp/Program.cs
@@ -84,7 +84,7 @@ namespace Robo_ConsoleApp
                             posicaoRoboy--;
                         }
 
-
+                        
                     }
 
                     //processamento
